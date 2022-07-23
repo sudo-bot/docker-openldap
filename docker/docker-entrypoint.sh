ba3e21e231676f9d5275ce7390a666bfc0cedcc1
William Desportes <williamdes@wdes.fr>
2022-07-24 01:48:33 +0200

    Replace the admin password in saslauhd.conf

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,6 +14,7 @@ set -eu
 ulimit -n $LDAP_NOFILE
 
 echo 'Replacing values with ENV values'
+sed -i "s|{{ LDAP_ADMIN_PASSWORD }}|${LDAP_ADMIN_PASSWORD}|" /etc/saslauthd.conf
 sed -i "s|{{ LDAP_AUTH_BASE_DN }}|${LDAP_AUTH_BASE_DN}|" /etc/saslauthd.conf
 sed -i "s|{{ LDAP_BASE_DN }}|${LDAP_BASE_DN}|" /etc/saslauthd.conf
 sed -i "s|{{ LDAP_AUTH_BASE_DN }}|${LDAP_AUTH_BASE_DN}|" /etc/openldap/slapd.conf
@@ -26,6 +27,7 @@ echo 'Checking if replacement worked'
 set -x
 grep -q -F "ldap_bind_dn: cn=admin,${LDAP_BASE_DN}" /etc/saslauthd.conf
 grep -q -F "ldap_search_base: ${LDAP_AUTH_BASE_DN}" /etc/saslauthd.conf
+grep -q -F "ldap_bind_pw: ${LDAP_ADMIN_PASSWORD}" /etc/saslauthd.conf
 grep -q -F ",${LDAP_AUTH_BASE_DN}" /etc/openldap/slapd.conf
 grep -q -F "suffix		\"${LDAP_BASE_DN}\"" /etc/openldap/slapd.conf
 grep -q -F "${LDAP_CONFIG_PASSWORD}" /etc/openldap/slapd.conf
@@ -35,3 +37,4 @@ set +x
 
 echo 'Starting...'
 horust --unsuccessful-exit-finished-failed
+ldap_bind_pw: {{ LDAP_ADMIN_PASSWORD }}
